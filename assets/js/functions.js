1c8c8503b7f4a4f2a8c8bd1512724614630446aa
jardelsouza2 <55919894+jardelsouza2@users.noreply.github.com>
2019-10-26 11:24:34 -0300

    trabalho

---
+++ assets/js/functions.js
@@ -0,0 +1,13 @@
+function supportsPlaceholder() {
+    return 'placeholder' in document.createElement('input') &&
+    'placeholder' in document.createElement('textarea');
+}
+jQuery(function($) {
+    function supportsPlaceholder() {
+    return 'placeholder' in document.createElement('input') &&
+    'placeholder' in document.createElement('textarea');
+    }
+    if(!supportsPlaceholder()) {
+        $('html').addClass('no-placeholder');
+        }
+});
